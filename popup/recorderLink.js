acc301b793996812d8245b7cb4e91667d8ac7791
Stephen Oney <swloney@gmail.com>
2015-09-05 11:46:11 -0400

    updated upload url

--- popup/recorderLink.js
+++ popup/recorderLink.js
@@ -91,7 +91,7 @@ function fieldGetter(fieldName) {
 function uploadScript(scriptInfo) {
 	return new Promise(function(resolve, reject) {
 		$.ajax({
-			url: RUNNER_PROTOCOL + '://' + RUNNER_HOST + ':' + RUNNER_PORT + '/uploadScript',
+			url: RUNNER_PROTOCOL + '://' + RUNNER_HOST + ':' + RUNNER_PORT + '/api/v1/uploadCommand',
 			method: 'POST',
 			data: scriptInfo,
 			success: function(response) {
